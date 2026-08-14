7ff3c3d9ce34dc79f9451464ddeaadaed1ce707b
divyanshu <divyanshusah44@gmail.com>
2026-08-14 20:22:22 +0530

    feat: update

--- project management saas/backend/src/controllers/projectController.js
+++ project management saas/backend/src/controllers/projectController.js
@@ -58,7 +58,7 @@ const updateProject = async (req, res) => {
         res.status(200).json({ message: "Project updated successfully" });
 
     }catch(err){
-        res.status(500).json({ message: "error updating project", error: err.message });
+        res.status(500).json({ message: "error updating project try again", error: err.message });
     }
 
 }
