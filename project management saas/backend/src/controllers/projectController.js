b64a4d563b8f91bfef3d2698986c554ce81a1093
divyanshu <divyanshusah44@gmail.com>
2026-08-14 20:21:36 +0530

    feat: delete

--- project management saas/backend/src/controllers/projectController.js
+++ project management saas/backend/src/controllers/projectController.js
@@ -44,12 +44,35 @@ const getProjectById = async (req, res) => {
 }
 
 
+const updateProject = async (req, res) => {
+    const projectId = req.params.id;
+    const userId = req.user.id;
+    const { name, description } = req.body;
 
+    try{
+        await projectModel.findOneAndUpdate(
+            { _id: projectId, user: userId },
+            { name, description },
+            { new: true }
+        );
+        res.status(200).json({ message: "Project updated successfully" });
 
+    }catch(err){
+        res.status(500).json({ message: "error updating project", error: err.message });
+    }
 
+}
 
+const deleteProject = async (req, res) => {
+    const projectId = req.params.id;
+    const userId = req.user.id;
 
+    try{
+        await projectModel.findOneAndDelete({ _id: projectId, user: userId });
+        res.status(200).json({ message: "Project deleted successfully" });
+    }catch(err){
+        res.status(500).json({ message: "error deleting project", error: err.message });
+    }
+}
 
-
-
-module.exports = { createProject , getProjects, getProjectById };
+module.exports = { createProject , getProjects, getProjectById, updateProject, deleteProject };
--- project management saas/backend/src/routes/projectRoute.js
+++ project management saas/backend/src/routes/projectRoute.js
@@ -6,7 +6,7 @@ const authMiddleware = require("../middlewares/authMiddleware")
 router.post("/create", authMiddleware, projectController.createProject)
 router.get("/get", authMiddleware, projectController.getProjects)
 router.get("/get/:id", authMiddleware, projectController.getProjectById)
-
-
+router.put("/update/:id", authMiddleware, projectController.updateProject)
+router.delete("/delete/:id", authMiddleware, projectController.deleteProject)
 
 module.exports = router
